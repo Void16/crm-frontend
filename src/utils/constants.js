cecd937a2902a962485824adf256dadf37595623
Void16 <olivmande@gmail.com>
2025-09-30 15:48:15 +0200

    hosting changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,4 @@
-export const API_BASE_URL = 'http://localhost:8000/api';
+// utils/constants.js
+
+// This pulls the base URL from your .env files
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
